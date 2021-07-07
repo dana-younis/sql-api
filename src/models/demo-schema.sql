efa548153d2ebfe03d5cdf7b6956eaa137796dfc
dana-younis <younisdana7@gmail.com>
2021-07-08 01:53:15 +0300

    9

--- src/models/demo-schema.sql
+++ src/models/demo-schema.sql
@@ -3,13 +3,12 @@ CREATE TABLE food(
   id SERIAL PRIMARY KEY,
   name varchar(255),
   price INTEGER,
-  catugary varchar(255)
-);
+  catugary varchar(255));
+
 
 DROP TABLE IF EXISTS clothes;
 CREATE TABLE clothes(
   id SERIAL PRIMARY KEY,
   name varchar(255),
   price INTEGER,
-  catugary varchar(255)
-);
+  catugary varchar(255));
