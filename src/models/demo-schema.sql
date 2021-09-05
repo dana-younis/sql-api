e2463aed2b437bdafdf0d738ff86aff9d662705a
dana younis <Younisdana7@gmail.com>
2021-09-05 14:06:55 +0300

    product

--- src/models/demo-schema.sql
+++ src/models/demo-schema.sql
@@ -3,8 +3,8 @@ CREATE TABLE Products(
   id SERIAL PRIMARY KEY,
   name varchar(255),
   price INTEGER,
-  catugary varchar(255));
- image varchar(255));
+  catugary varchar(255)),
+ image varchar(255)),
   description varchar(255));
 
 DROP TABLE IF EXISTS clothes;
